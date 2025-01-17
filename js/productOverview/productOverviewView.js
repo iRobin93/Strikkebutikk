ad3e5d06d157019082e07e43ec21cba1cb009376
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2025-01-17 11:17:37 +0100

    x

--- js/productOverview/productOverviewView.js
+++ js/productOverview/productOverviewView.js
@@ -1,1 +1,39 @@
-function updateViewOverview() {}
+function updateViewOverview() 
+{
+    let html = "";
+    html += "<table>";
+    html += /*HTML*/ `
+                    <table>
+                        <tr>
+                            <th>Oppgave</th>
+                            <th>Person</th>
+                            <th>Frist</th>
+                            <th>Gjort</th>
+                            <th></th>
+                            <th></th>
+                            
+                        </tr>`;
+
+
+    for(let i = 0; i < model.data.products.length; i++)
+    {
+        html += createHtmlRowOverview(i);
+    }
+    html += "</table>";
+
+
+    app.innerHTML += html;
+}
+
+function createHtmlRowOverview(i)
+{
+    let product = model.data.products[i];
+    return /*HTML*/ `
+                        <tr>
+                            <td>${product.productImg}</td>
+                        </tr>
+    
+    
+    
+    `;
+}
